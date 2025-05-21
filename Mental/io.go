d0da9da3370f6ce967ee8fb71c400b1cd341ebd1
Muhammad Fauzan <111763204+mhmmdffzz@users.noreply.github.com>
2025-05-21 15:17:02 +0700

    Add files via upload

---
+++ Mental/io.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func InputJawabanKuesioner(jumlahPertanyaan int) []int {
+	jawaban := make([]int, jumlahPertanyaan)
+	for i := 0; i < jumlahPertanyaan; i++ {
+		for {
+			fmt.Printf("Pertanyaan %d: ", i+1)
+			var input int
+			fmt.Scan(&input)
+			if input >= 1 && input <= 5 {
+				jawaban[i] = input
+				break
+			} else {
+				fmt.Println("Error: Masukkan angka 1-5")
+			}
+		}
+	}
+	return jawaban
+}
+
+func HitungTotalSkor(jawaban []int) int {
+	total := 0
+	for _, nilai := range jawaban {
+		total += nilai
+	}
+	return total
+}
+
+func FormatTanggal(tanggalString string) time.Time {
+	tanggal, _ := time.Parse("02-01-2006", tanggalString)
+	return tanggal
+}
